4cbf4cdb7dba8e6747e30c88627afa4d4569608a
ratchapon prangthong <ppete2547@gmail.com>
2022-12-20 16:13:48 +0700

    lab8_2 fish

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -16,3 +16,42 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+string name,movie,day,talk;
+int num,N;
+
+
+cout << "Fahsai: Sawadee ka...Can you tell me your name?" << endl;
+cout << "?????: ";
+getline(cin,name);
+cout << "Fahsai: Wow!!! " << name << " is a really cool name." << endl;
+cout << "Fahsai: I think you are an Engineering student. What is your student ID?" << endl;
+cout << name << ": ";
+cin >> num;
+cin.ignore();
+N = num/10000000;
+cout << "Fahsai: I think you may be GEAR " << N-12 << ". I have a free movie ticket for you." << endl;
+cout <<"Fahsai: Let's go to the cinema together!!!" << endl;
+cout << "Fahsai: What movie do you want to watch?" << endl;
+cout << name << ": ";
+getline(cin,movie);
+
+cout <<"Fahsai: So....which day are you free to go with me?" << endl;
+cout << name << ": ";
+getline(cin,day);
+
+cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you." << endl;
+cout << name << ": ";
+
+
+cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+}
+
+
+
+
+
